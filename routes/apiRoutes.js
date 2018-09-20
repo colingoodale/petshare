be873c72b1b69bb49407a5f2e65c4c0c57037621
GJDeY <gregoryjdeyoung@u.northwestern.edu>
2018-09-20 14:07:42 -0500

    stripe info & .env file added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,29 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/examples", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 };
+
+// stripe payment API test - example request
+stripe.charges.retrieve("ch_1DCWlH2eZvKYlo2CXLJZ7CMk", {
+  api_key: "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+});
