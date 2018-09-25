d728810e7191ba18be05fed6fa0afd8d6a67d7b5
Colin <colingoodale@gmail.com>
2018-09-24 21:27:02 -0500

    deleted superfolous routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,19 +6,6 @@ module.exports = function (app) {
     res.json("user-info");
   });
 
-  // Create a new example
-  app.post("/api/examples", function (req, res) {
-    db.Example.create(req.body).then(function (dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function (req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.json(dbExample);
-    });
-  });
 
   app.post("/api/sign-up", function (req, res) {
     db.User.create({
